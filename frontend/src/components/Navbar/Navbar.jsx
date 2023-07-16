4b3c2a81ae9fe3e6747bbfb738d189d9fbe36977
Shahedkhalaileh <khalaileh.shahed@gmail.com>
2023-07-16 00:43:40 -0700

    toggle navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,9 +2,14 @@ import { NavLink, Outlet } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Nav from "./Navbar.module.css";
+import menu from "../../images/menu.png";
+import cancel from "../../images/cancel.png";
+
 function Navbar() {
   const [active, setActive] = useState(false);
+  const [toggleNav, setToggleNav] = useState(false);
   const location = useLocation();
+  let obj = toggleNav ? { display: "flex" } : {};
   useEffect(() => {
     if (location.pathname === "/terms") {
       setActive(true);
@@ -13,38 +18,61 @@ function Navbar() {
   }, []);
 
   return (
-    <nav className={`${Nav.navbar} ${active ? Nav.active : ""}`}>
-      <div className={Nav.left}>Logo</div>
-      <div className="left">
-        <ul className={Nav.list}>
-          <li>
-            <NavLink to="/" className={Nav.home} href="">
-              Home
-            </NavLink>
-          </li>
+    <>
+      <div>
+        <nav
+          className={`${Nav.navbar} ${active ? Nav.active : ""}`}
+          style={obj}
+        >
+          <div className={Nav.left}>Logo</div>
+          <div className="left">
+            <ul className={Nav.list}>
+              <li>
+                <NavLink to="/" className={Nav.home} href="">
+                  Home
+                </NavLink>
+              </li>
 
-          <li>
-            <a href="#about">About</a>
-          </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
 
-          <li>
-            <NavLink to="terms" href="">
-              Terms & Conditions
-            </NavLink>
-          </li>
+              <li>
+                <NavLink to="terms" href="">
+                  Terms & Conditions
+                </NavLink>
+              </li>
 
-          <li>
-            <a href="#contact">Contact us</a>
-          </li>
-        </ul>
+              <li>
+                <a href="#contact">Contact us</a>
+              </li>
+            </ul>
+          </div>
+          <div className={Nav.sign}>
+            <a href="" className={Nav.icon}>
+              <i class="fa fa-cart-plus"></i>
+            </a>
+            <button className={Nav.btn}>Sign in</button>
+          </div>
+        </nav>
       </div>
-      <div className={Nav.sign}>
-        <a href="" className={Nav.icon}>
-          <i class="fa fa-cart-plus"></i>
-        </a>
-        <button className={Nav.btn}>Sign in</button>
-      </div>
-    </nav>
+      {!toggleNav && (
+        <img
+          className={Nav.menu}
+          src={menu}
+          alt="menu"
+          onClick={() => setToggleNav(true)}
+        />
+      )}
+      {toggleNav && (
+        <img
+          className={Nav.cancel}
+          src={cancel}
+          alt="cancel"
+          onClick={() => setToggleNav(false)}
+        />
+      )}
+    </>
   );
 }
 export default Navbar;
