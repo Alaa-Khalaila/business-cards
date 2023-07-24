6f09abe9e3c19ad26730128aecba8db0abbc32d2
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2023-07-25 01:03:49 +0300

    update data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import About from "../components/About/About";
 import Cards from "../components/Cards/Cards";
 import Contact from "../components/Contact/Contact";
 import Hero from "../components/Hero/Hero";
-import Footer from "../components/footer/Footer";
 import card1 from "../images/1.png";
 import card2 from "../images/2.png";
 import card3 from "../images/3.png";
@@ -15,32 +14,41 @@ import card9 from "../images/9.jpeg";
 
 function Home() {
   const cards = [
-    {
+    {id:1,
       img: card7,
+      category:"1"
     },
-    {
+    {id:2,
       img: card2,
+      category:"2"
     },
-    {
+    {id:3,
       img: card3,
+      category:"3"
     },
-    {
+    {id:4,
       img: card4,
+      category:"1"
     },
-    {
+    {id:5,
       img: card6,
+      category:"3"
     },
-    {
+    {id:6,
       img: card9,
+      category:"2"
     },
-    {
+    {id:7,
       img: card1,
+      category:"2"
     },
-    {
+    {id:8,
       img: card8,
+      category:"1"
     },
-    {
+    {id:9,
       img: card5,
+      category:"3"
     },
   ];
   return (
