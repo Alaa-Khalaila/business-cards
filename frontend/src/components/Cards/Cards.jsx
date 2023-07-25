01cd2de1266d8bd2f19ea00ad18a9ab4023ac3c2
Shahedkhalaileh <khalaileh.shahed@gmail.com>
2023-07-25 06:55:38 -0700

    add category

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -1,29 +1,24 @@
 import Card from "../Card/Card";
-import styles from "./Cards.module.css"
-import logo from "../../images/mit_logo.png"
-import { useTranslation } from "react-i18next";
+import styles from "./Cards.module.css";
+import logo from "../../images/mit_logo.png";
 
-function Cards(props){
-
-  const [tranlate] = useTranslation("global");
-    const listItems = props.cards.map((card) =>
+function Cards(props) {
+  const listItems = props.cards.map((card) => (
     <li>
-      <Card title={card.title} description={card.description} img={card.img}></Card>
+      <Card
+        title={card.title}
+        description={card.description}
+        img={card.img}
+      ></Card>
     </li>
+  ));
+  return (
+    <div className="container">
+      <ul id="cards" className={styles.cards}>
+        {listItems}
+      </ul>
+    </div>
   );
-    return(
-      <div className="container">
-        <div className={styles.conditions}>
-          <img src={logo} alt="" />
-          <div > {tranlate("Cards.conditions")}</div>
-
-        </div>
-              <ul id="cards" className={styles.cards}>
-            {listItems}
-        </ul>
-        </div>
-
-    )
 }
 
 export default Cards;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import About from "../components/About/About";
+import Button from "../components/Button/Button";
 import Cards from "../components/Cards/Cards";
 import Contact from "../components/Contact/Contact";
 import Hero from "../components/Hero/Hero";
@@ -11,50 +12,74 @@ import card6 from "../images/6.png";
 import card7 from "../images/7.jpeg";
 import card8 from "../images/8.jpeg";
 import card9 from "../images/9.jpeg";
+import { useEffect, useState } from "react";
+import home from "./Home.module.css";
+import logo from "../images/mit_logo.png";
+import { useTranslation } from "react-i18next";
 
 function Home() {
   const cards = [
-    {id:1,
-      img: card7,
-      category:"1"
-    },
-    {id:2,
-      img: card2,
-      category:"2"
-    },
-    {id:3,
-      img: card3,
-      category:"3"
-    },
-    {id:4,
-      img: card4,
-      category:"1"
-    },
-    {id:5,
-      img: card6,
-      category:"3"
-    },
-    {id:6,
-      img: card9,
-      category:"2"
-    },
-    {id:7,
-      img: card1,
-      category:"2"
-    },
-    {id:8,
-      img: card8,
-      category:"1"
-    },
-    {id:9,
-      img: card5,
-      category:"3"
-    },
+    { id: 1, img: card7, category: "اعلام" },
+    { id: 2, img: card2, category: "اعمال" },
+    { id: 3, img: card3, category: "اعلام" },
+    { id: 4, img: card4, category: "حرف" },
+    { id: 5, img: card6, category: "اعلام" },
+    { id: 6, img: card9, category: "اعمال" },
+    { id: 7, img: card1, category: "اعمال" },
+    { id: 8, img: card8, category: "حرف" },
+    { id: 9, img: card5, category: "اعلام" },
   ];
+  const [active, setActive] = useState();
+  const [tranlate] = useTranslation("global");
+  const [data, setData] = useState(cards);
+  const [collection, setCollection] = useState([]);
+
+  useEffect(() => {
+    setData(data);
+    setCollection([...new Set(cards.map((item) => item.category))]);
+  }, []);
+
+  const FilterItems = (itemData) => {
+    const filterData = cards.filter((item) => item.category == itemData);
+    setData(filterData);
+  };
+
   return (
     <>
       <Hero />
-      <Cards cards={cards}></Cards>
+      <div>
+        <div>
+          <div className={home.conditions}>
+            <img src={logo} alt="" />
+            <div> {tranlate("Cards.conditions")}</div>
+          </div>
+          <ul className={home.filterItem}>
+            <li>
+              <button
+                onClick={() => {
+                  setData(cards);
+                }}
+              >
+                الكل
+              </button>
+            </li>
+            {collection.map((item) => (
+              <li>
+                <button
+                  onClick={() => {
+                    FilterItems(item);
+                  }}
+                >
+                  {item}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Cards cards={data}></Cards>
+
+        <div></div>
+      </div>
       <About></About>
       <Contact></Contact>
     </>
