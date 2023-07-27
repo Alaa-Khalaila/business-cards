8e600145d5d4c4e77721d691b111aa28a8ec45e1
Shahedkhalaileh <khalaileh.shahed@gmail.com>
2023-07-27 06:47:52 -0700

    edit cart

--- frontend/src/components/CartItem/CartItem.jsx
+++ frontend/src/components/CartItem/CartItem.jsx
@@ -1,16 +1,30 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import CartContext from "../../CartContex";
 import styles from "./CartItem.module.css";
-
+import Button from "../Button/Button";
 
 function CartItem(props) {
-    const {cartItems} = useContext(CartContext)
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className={styles.cart__item}>
-      <div><img className={styles.cart__item__img} src={props.product.img} alt="" srcset="" /> </div>
+      <div>
+        <img
+          className={styles.cart__item__img}
+          src={props.product.img}
+          alt=""
+          srcset=""
+        />{" "}
+      </div>
       <div>1JD</div>
-      <div>{cartItems[props.product.id]}</div>
-      <div>{cartItems[props.product.id]+"JD"}</div>
+
+      <div className={styles.btns}>
+        <Button classList="red" text="-"></Button>
+        <span className={styles.qunt}>{cartItems[props.product.id]}</span>
+        <Button classList="yellow" text="+"></Button>
+      </div>
+
+      <div>{cartItems[props.product.id] + "JD"}</div>
     </div>
   );
 }
